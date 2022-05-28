56315f3535593f42808827ca26a6bd88516700f9
efrank <frankehud@gmail.com>
2022-05-28 19:12:49 +0200

    Make bunny move with arrows

--- game.py
+++ game.py
@@ -6,6 +6,8 @@ from pygame.locals import *
 pygame.init()
 width, height = 640, 480
 screen=pygame.display.set_mode((width, height))
+keys = [False, False, False, False]
+playerpos=[100,100]
 
 # 3 - Load images
 player = pygame.image.load("resources/images/dude.png")
@@ -25,13 +27,40 @@ while 1:
     screen.blit(castle,(0,240))
     screen.blit(castle,(0,345 ))
 
-    screen.blit(player, (100,100))
+    screen.blit(player, playerpos)
     # 7 - update the screen
     pygame.display.flip()
     # 8 - loop through the events
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key==K_UP:
+                keys[0]=True
+            elif event.key==K_LEFT:
+                keys[1]=True
+            elif event.key==K_DOWN:
+                keys[2]=True
+            elif event.key==K_RIGHT:
+                keys[3]=True
+        if event.type == pygame.KEYUP:
+            if event.key==pygame.K_UP:
+                keys[0]=False
+            elif event.key==pygame.K_LEFT:
+                keys[1]=False
+            elif event.key==pygame.K_DOWN:
+                keys[2]=False
+            elif event.key==pygame.K_RIGHT:
+                keys[3]=False
         # check if the event is the X button 
         if event.type==pygame.QUIT:
             # if it is quit the game
             pygame.quit() 
             exit(0)
+    # 9 - Move player
+    if keys[0]:
+        playerpos[1]-=1
+    elif keys[2]:
+        playerpos[1]+=1
+    if keys[1]:
+        playerpos[0]-=1
+    elif keys[3]:
+        playerpos[0]+=1
