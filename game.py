6c3e253afb7b9fb851c5523796e3193fe3fa56fd
efrank <frankehud@gmail.com>
2022-05-29 11:58:12 +0200

    Make natchobacho shoot arrows

--- game.py
+++ game.py
@@ -11,12 +11,15 @@ nachobachoWidth, nachobachoHeight = 66, 78
 screen=pygame.display.set_mode((width, height))
 keys = [False, False, False, False, False, False]
 playerpos=[100,100]
+angle = 0
+acc=[0,0]
+arrows=[]
 
 # 3 - Load images
 player = pygame.image.load("resources/images/natchobacho.png")
 grass = pygame.image.load("resources/images/grass.png")
 castle = pygame.image.load("resources/images/castle.png")
-angle = 0
+arrow = pygame.image.load("resources/images/bullet.png")
 
 # 4 - keep looping through
 while 1:
@@ -32,12 +35,23 @@ while 1:
     screen.blit(castle,(0,345 ))
 
     # 6.1 - Set player position and rotation
-    # position = pygame.mouse.get_pos()
-    # angle = math.atan2(position[1]-(playerpos[1]+32),position[0]-(playerpos[0]+26))
-    # playerrot = pygame.transform.rotate(player, 360-angle*57.29)
     playerrot = pygame.transform.rotate(player, angle)
     playerpos1 = (playerpos[0]-playerrot.get_rect().width/2, playerpos[1]-playerrot.get_rect().height/2)
-    screen.blit(playerrot, playerpos1) 
+    screen.blit(playerrot, playerpos1)
+
+    # 6.2 - Draw arrows
+    for bullet in arrows:
+        index=0
+        velx=math.cos(bullet[0])*10
+        vely=math.sin(bullet[0])*10
+        bullet[1]+=velx
+        bullet[2]+=vely
+        if bullet[1]<-64 or bullet[1]>640 or bullet[2]<-64 or bullet[2]>480:
+            arrows.pop(index)
+        index+=1
+        for projectile in arrows:
+            arrow1 = pygame.transform.rotate(arrow, 360-projectile[0]*57.29)
+            screen.blit(arrow1, (projectile[1], projectile[2]))
 
     # 7 - update the screen
     pygame.display.flip()
@@ -56,6 +70,10 @@ while 1:
                 keys[4]=True
             elif event.key==K_x:
                 keys[5]=True
+            elif event.key == K_SPACE:
+                position=pygame.mouse.get_pos()
+                acc[1]+=1
+                arrows.append([-angle/57.29, playerpos1[0]+40, playerpos1[1]+32])
         if event.type == pygame.KEYUP:
             if event.key==pygame.K_UP:
                 keys[0]=False
@@ -69,6 +87,7 @@ while 1:
                 keys[4]=False
             elif event.key==K_x:
                 keys[5]=False
+
         # check if the event is the X button 
         if event.type==pygame.QUIT:
             # if it is quit the game
