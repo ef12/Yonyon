f1ed43be46fb8bba602644344e37a60ef175a94c
efrank <frankehud@gmail.com>
2022-05-31 21:27:30 +0200

    Firt working game

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
 # 1 - Import library
-import math
-import pygame, math
+import pygame, math, random
 from pygame.locals import *
 
 # 2 - Initialize the game
@@ -15,14 +14,44 @@ angle = 0
 acc=[0,0]
 arrows=[]
 
+badtimer=100
+badtimer1=0
+badguys=[[640,100]]
+healthvalue=194
+
+pygame.mixer.init()
+
 # 3 - Load images
 player = pygame.image.load("resources/images/natchobacho.png")
 grass = pygame.image.load("resources/images/grass.png")
 castle = pygame.image.load("resources/images/castle.png")
-arrow = pygame.image.load("resources/images/bullet.png")
+arrow = pygame.image.load("resources/images/natcho.png")
+
+badguyimg1 = pygame.image.load("resources/images/badguy.png")
+badguyimg=badguyimg1
+
+healthbar = pygame.image.load("resources/images/healthbar.png")
+health = pygame.image.load("resources/images/health.png")
+
+gameover = pygame.image.load("resources/images/gameover.png")
+youwin = pygame.image.load("resources/images/youwin.png")
+
+# 3.1 - Load audio
+hit = pygame.mixer.Sound("resources/audio/explode.wav")
+enemy = pygame.mixer.Sound("resources/audio/enemy.wav")
+shoot = pygame.mixer.Sound("resources/audio/shoot.wav")
+hit.set_volume(0.05)
+enemy.set_volume(0.05)
+shoot.set_volume(0.05)
+pygame.mixer.music.load('resources/audio/moonlight.wav')
+pygame.mixer.music.play(-1, 0.0)
+pygame.mixer.music.set_volume(0.25)
 
 # 4 - keep looping through
-while 1:
+running = 1
+exitcode = 0
+while running:
+    badtimer-=1
     # 5 - clear the screen before drawing it again
     screen.fill(0)
     # 6 - draw the screen elements
@@ -53,6 +82,57 @@ while 1:
             arrow1 = pygame.transform.rotate(arrow, 360-projectile[0]*57.29)
             screen.blit(arrow1, (projectile[1], projectile[2]))
 
+    # 6.3 - Draw badgers
+    if badtimer==0:
+        badguys.append([640, random.randint(50,430)])
+        badtimer=100-(badtimer1*2)
+        if badtimer1>=35:
+            badtimer1=35
+        else:
+            badtimer1+=1
+    index=0
+    for badguy in badguys:
+        if badguy[0]<-64:
+            badguys.pop(index)
+        badguy[0]-=1
+        # 6.3.1 - Attack castle
+        badrect=pygame.Rect(badguyimg.get_rect())
+        badrect.top=badguy[1]
+        badrect.left=badguy[0]
+        if badrect.left<64:
+            hit.play()
+            healthvalue -= random.randint(5,20)
+            badguys.pop(index)
+        #6.3.2 - Check for collisions
+        index1=0
+        for bullet in arrows:
+            bullrect=pygame.Rect(arrow.get_rect())
+            bullrect.left=bullet[1]
+            bullrect.top=bullet[2] 
+            if badrect.colliderect(bullrect):
+                enemy.play()
+                acc[0]+=1
+                badguys.pop(index)
+                arrows.pop(index1)
+            index1+=1
+        # 6.3.3 - Next bad guy        
+        index+=1
+ 
+    for badguy in badguys:
+        screen.blit(badguyimg, badguy)
+    # 6.4 - Draw clock
+    font = pygame.font.Font(None, 24)
+    survivedtext = font.render(str(round((90000-
+    pygame.time.get_ticks())/60000))+":"+str(round((90000-
+    pygame.time.get_ticks())/1000%60)).zfill(2), True, (0,0,0))
+    textRect = survivedtext.get_rect()
+    textRect.topright=[635,5]
+    screen.blit(survivedtext, textRect)
+    # 6.5 - Draw health bar
+    screen.blit(healthbar, (5,5))
+    for health1 in range(healthvalue):
+        screen.blit(health, (health1+8,8))
+
     # 7 - update the screen
     pygame.display.flip()
     # 8 - loop through the events
@@ -71,7 +151,7 @@ while 1:
             elif event.key==K_x:
                 keys[5]=True
             elif event.key == K_SPACE:
-                position=pygame.mouse.get_pos()
+                shoot.play()
                 acc[1]+=1
                 arrows.append([-angle/57.29, playerpos1[0]+40, playerpos1[1]+32])
         if event.type == pygame.KEYUP:
@@ -96,17 +176,54 @@ while 1:
     # 9 - Move player
     if keys[0]:
         if (nachobachoHeight/2) <= playerpos[1]:
-            playerpos[1]-=1
+            playerpos[1]-=2
     elif keys[2]:
         if (height - (nachobachoHeight/2)) >= playerpos[1]:
-            playerpos[1]+=1
+            playerpos[1]+=2
     if keys[1]:
         if (nachobachoWidth/2) <= playerpos[0]:
-            playerpos[0]-=1
+            playerpos[0]-=2
     elif keys[3]:
         if (width - (nachobachoWidth/2)) >= playerpos[0]:
-            playerpos[0]+=1
+            playerpos[0]+=2
     elif keys[4]:
-        angle += 0.5
+        angle += 0.7
     elif keys[5]:
-        angle -= 0.5
+        angle -= 0.7
+
+    #10 - Win/Lose check
+    if pygame.time.get_ticks()>=90000:
+        running=0
+        exitcode=1
+    if healthvalue<=0:
+        running=0
+        exitcode=0
+    if acc[1]!=0:
+        accuracy=acc[0]*1.0/acc[1]*100
+    else:
+        accuracy=0
+# 11 - Win/lose display        
+if exitcode==0:
+    pygame.font.init()
+    font = pygame.font.Font(None, 24)
+    text = font.render("Accuracy: "+str(round(accuracy))+"%", True, (255,0,0))
+    textRect = text.get_rect()
+    textRect.centerx = screen.get_rect().centerx
+    textRect.centery = screen.get_rect().centery+24
+    screen.blit(gameover, (0,0))
+    screen.blit(text, textRect)
+else:
+    pygame.font.init()
+    font = pygame.font.Font(None, 24)
+    text = font.render("Accuracy: "+str(round(accuracy))+"%", True, (0,255,0))
+    textRect = text.get_rect()
+    textRect.centerx = screen.get_rect().centerx
+    textRect.centery = screen.get_rect().centery+24
+    screen.blit(youwin, (0,0))
+    screen.blit(text, textRect)
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit(0)
+    pygame.display.flip()
